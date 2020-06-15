9ad4821ed78e440496518b9f3b6379a0049a1501
github username <github email address>
2020-06-15 13:32:02 -0600

    Create database seeds.

--- imThirsty/db/seeds.rb
+++ imThirsty/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+UserCocktail.destroy_all
+User.destroy_all
+
+carlos = User.create(name: 'Carlos', username: 'carlos_the_awesome', password: 'password')
+bryce = User.create(name: 'Bryce', username: 'brycius_maximus', password: 'password')
+
+UserCocktail.create(user: bryce, cocktail_id: 16_134)
+UserCocktail.create(user: bryce, cocktail_id: 11_113)
+UserCocktail.create(user: carlos, cocktail_id: 13_200)
+UserCocktail.create(user: carlos, cocktail_id: 12_718)
