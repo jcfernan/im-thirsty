0935a3ac0e3060cda380e7e325cb07e2508aab64
github username <github email address>
2020-06-17 15:51:11 -0600

    Finish styling index and user pages. Create flip cards with cocktail ingredients and animate.

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,11 @@
 const searchParams = new URLSearchParams(window.location.search)
 const drink = searchParams.get('search')
 
-
 if (drink) {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(renderJson)
     .then(renderSearchResults)
-} 
+}
 
 function renderJson(data) {
   return data.json()
@@ -19,9 +18,23 @@ function renderSearchResults(searchResults) {
 function mapSearchResults(searchResults) {
   return searchResults.drinks.map(drink => {
     const $li = document.createElement('li')
+    $li.classList.add("outer-container")
     $li.innerHTML = `
-    <p>${drink.strDrink}</p>
-    <img src="${drink.strDrinkThumb}">`
+    <div class="inner-container">
+      <div class="card-front">
+        <p class="drink-name">${drink.strDrink}</p>
+        <img src="${drink.strDrinkThumb}">
+        <p>Click to make it at home</p>
+      </div>
+      <div class="card-back">
+        <p>${drink.strDrink}</p>
+        <ul class="ingredients">${getDrinkIngredients(drink)}</ul>
+        <p>${drink.strInstructions}</p>
+      </div>
+    </div>`
+    $li.addEventListener("click", () => {
+      $li.classList.toggle("reverse")
+    })
     return $li
   })
 }
@@ -33,10 +46,22 @@ function appendDrinks(drinks) {
   })
 }
 
-function randomDrink () {
-
-document.querySelector("#results").innerHTML = ''
-fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-.then(renderJson)
-.then(renderSearchResults)
+function getDrinkIngredients(drink) {
+  let ingredients = ''
+  for (i = 1; i <= 15; i++) {
+    if (drink[`strIngredient${i}`]) {
+      const ingredient = drink[`strIngredient${i}`]
+      const measure = drink[`strMeasure${i}`]
+      ingredients += `<li>${ingredient} - ${measure}</li>`
+    }
+  }
+  return ingredients
 }
+
+function randomDrink() {
+  document.querySelector('#results').innerHTML = ''
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(renderJson)
+    .then(renderSearchResults)
+}
+
--- frontend/user.js
+++ frontend/user.js
@@ -34,14 +34,28 @@ function renderSearchResults(searchResults) {
 function mapSearchResults(searchResults) {
   return searchResults.drinks.map(drink => {
     const $li = document.createElement('li')
+    $li.classList.add("outer-container")
     $li.innerHTML = `
-    <p>${drink.strDrink}</p>
-    <img src="${drink.strDrinkThumb}">
+    <div class="inner-container">
+      <div class="card-front">
+        <p class="drink-name">${drink.strDrink}</p>
+        <img src="${drink.strDrinkThumb}">
+        <p>Click to make it at home</p>
+      </div>
+      <div class="card-back">
+        <p>${drink.strDrink}</p>
+        <ul class="ingredients">${getDrinkIngredients(drink)}</ul>
+        <p>${drink.strInstructions}</p>
+      </div>
+    </div>
     <form method="POST" action="http://localhost:3001/user_cocktails">
-      <input type="hidden" name="user_id" value="${id}">
-      <input type="hidden" name="cocktail_id" value="${drink.idDrink}">
-      <input class="favorite-button" type="submit" value="Add Favorite">
+    <input type="hidden" name="user_id" value="${id}">
+    <input type="hidden" name="cocktail_id" value="${drink.idDrink}">
+    <input class="favorite-button" type="submit" value="Add Favorite">
     </form>`
+    $li.addEventListener("click", () => {
+      $li.classList.toggle("reverse")
+    })
     return $li
   })
 }
@@ -66,14 +80,49 @@ function renderFavorites(userData) {
   })
 }
 
-function createFavorite(drinkData, cocktailId) {
+function getDrinkIngredients(drink) {
+  let ingredients = ''
+  for (i = 1; i <= 15; i++) {
+    if (drink[`strIngredient${i}`]) {
+      const ingredient = drink[`strIngredient${i}`]
+      const measure = drink[`strMeasure${i}`]
+      ingredients += `<li>${ingredient} - ${measure}</li>`
+    }
+  }
+  return ingredients
+}
+
+function createFavorite(drink, cocktailId) {
+  const drinkInfo = drink.drinks[0]
   const $li = document.createElement('li')
+  $li.classList.add("outer-container")
   $li.innerHTML = `
-    <p>${drinkData.drinks[0].strDrink}</p>
-    <img src="${drinkData.drinks[0].strDrinkThumb}">
-    <form method="POST" action="http://localhost:3001/user_cocktails/${cocktailId}">
+  <div class="inner-container">
+    <div class="card-front">
+      <p class="drink-name">${drinkInfo.strDrink}</p>
+      <img src="${drinkInfo.strDrinkThumb}">
+      <p>Click to make it at home</p>
+    </div>
+    <div class="card-back">
+      <p>${drinkInfo.strDrink}</p>
+      <ul class="ingredients">${getDrinkIngredients(drinkInfo)}</ul>
+      <p>${drinkInfo.strInstructions}</p>
+    </div>
+    </div>
+    <form method = "POST" action = "http://localhost:3001/user_cocktails/${cocktailId}">
       <input type="hidden" name="_method" value="DELETE">
         <input class="favorite-button" type="submit" value="Remove Favorite">
-    </form>`
+  </form>`
+  $li.addEventListener("click", () => {
+    $li.classList.toggle("reverse")
+  })
   document.querySelector('#favorites').append($li)
+}
+
+
+function randomDrink() {
+  document.querySelector('#results').innerHTML = ''
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(renderJson)
+    .then(renderSearchResults)
 }
